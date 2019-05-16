de1f271c21f4dc151c95bea7fbc7a496ba80e07d
tstiddy <spaceinvadini99@gmail.com>
2019-05-16 08:51:35 +0100

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(total = 0)
+    @total = total
+  end
+
+  def cash_register_with_discount(discount)
+    @discount = 20
+    class
+  end
+
+end
